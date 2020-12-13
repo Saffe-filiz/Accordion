7148248dd9865bf039fced54e03273117421fb07
Saffe-filiz <saffetfiliz1989@gmail.com>
2020-12-13 06:48:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,48 @@
 
-Vue.component('faq', {
-	props: ['question', 'index', 'open'],
-	template: `
-	<div :class="[ open ? 'questions-on': 'questions-off']" @click="$emit('chosen_question', index)">
-	 <span class="title"><h1 class="question">{{question.question}}</h1></span>
-	  <h3 class="answer" v-html="space(question.answer)"></h3>
-	</div>
-	`,
+Vue.component('windows', {
+  props: {
+    'questions': Object,
+  },
+  template: `
+      <span>
+          <span class="list">
+            <ul>
+              <li v-for="(w, index) in questions" :class="[w.open ? 'active': '']"  @click="$emit('chosen_question', index)">
+               <h3>{{w.question}}</h3>
+                <p>{{w.answer}} </p>
+                <button>Learn more</button>
+             </li>
+            </ul>
+          </span>
+      </span>
+  `,
 
-	methods: {
-		space (str) {
-			return str.replace(/\n/gi, '<br>')
-		}
-	}
 })
 
 
 var app = new Vue({
-  el: '#app',
+  el: '.container',
 
   data: {
     questions: [
         {
-       	    question: 'What is Lorem Ipsum ?',
-       	    answer: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.\n \n It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+       	    question: 'Why this term ?',
+       	    answer: 'Several windows are stacked on each other. All of them are "shaded", so only their captions are visible. If one of them is clicked, to make it active, it is "unshaded" or "maximized". Other windows in accordion are displaced around top or bottom edge.',
        	    open: false,
         },
         {
-        	  question: 'Why do we use it ?',
-       	    answer: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English.\n\n Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+            question: 'When to use accordion components ?',
+       	    answer: 'Several windows are stacked on each other. All of them are "shaded", so only their captions are visible. If one of them is clicked, to make it active, it is "unshaded" or "maximized". Other windows in accordion are displaced around top or bottom edge.',
        	    open: false,
         },
         {
-       	    question: 'Where does it come from ?',
-       	    answer: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.\n\n Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.',
+       	    question: 'How can it be defined ?',
+       	    answer: 'Several windows are stacked on each other. All of them are "shaded", so only their captions are visible. If one of them is clicked, to make it active, it is "unshaded" or "maximized". Other windows in accordion are displaced around top or bottom edge.',
        	    open: false,
         },
         {
-            question: 'Where can I get some ?',
-            answer: 'here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. \n\n It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.',
+            question: 'When to use accordion components ?',
+            answer: 'Several windows are stacked on each other. All of them are "shaded", so only their captions are visible. If one of them is clicked, to make it active, it is "unshaded" or "maximized". Other windows in accordion are displaced around top or bottom edge.',
             open: false,
         }
     ]
@@ -46,15 +50,16 @@ var app = new Vue({
 
   methods: {
   	chosen_question (index) {
-  		this.questions = this.questions.map( (v,i) => {
-  			if(index == i){
-  				v.open = !v.open;
-  			}else {
-  				v.open = false
-  			}
-  			return v;
-  		})
-  	}
+  	this.questions = this.questions.map( (v,i) => {
+  	if(index == i){
+  	v.open = !v.open;
+  	}else {
+         v.open = false;
+        }
+  	return v;
+         })
+        }
   }
 });
+
 
